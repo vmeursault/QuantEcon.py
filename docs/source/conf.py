7a95fd2c7f53577203b2c13c5d9dfa6d42c11983
Matthew McKay <mamckay@gmail.com>
2015-05-04 22:24:01 +1000

    remove mock to test readthedocs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -16,28 +16,28 @@ import sys
 import os
 import pip
 
-try:
-    from mock import Mock as MagicMock
-except ImportError:
-    def install(package):
-        pip.main(['install', package])
+# try:
+#     from mock import Mock as MagicMock
+# except ImportError:
+#     def install(package):
+#         pip.main(['install', package])
 
-    install('mock')
-    from mock import Mock as MagicMock
+#     install('mock')
+#     from mock import Mock as MagicMock
 
-# ------------------------------------------------------------------- #
-# MOCK MODULES
-# ------------------------------------------------------------------- #
-on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
+# # ------------------------------------------------------------------- #
+# # MOCK MODULES
+# # ------------------------------------------------------------------- #
+# on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
 
-if on_rtd:
-    class Mock(MagicMock):
-        @classmethod
-        def __getattr__(cls, name):
-            return Mock()
+# if on_rtd:
+#     class Mock(MagicMock):
+#         @classmethod
+#         def __getattr__(cls, name):
+#             return Mock()
 
-    MOCK_MODULES = ['pandas', 'statsmodels', 'sympy', "sympy.mpmath", 'numba']
-    sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
+#     MOCK_MODULES = ['pandas', 'statsmodels', 'sympy', "sympy.mpmath", 'numba']
+#     sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
